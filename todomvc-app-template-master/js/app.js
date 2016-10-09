372930fbee2f4df8eb0749ad1a33c0d8fba2f95d
SHY <shyang292@gmail.com>
2016-10-09 11:58:53 -0700

    $location实现

--- todomvc-app-template-master/js/app.js
+++ todomvc-app-template-master/js/app.js
@@ -3,7 +3,7 @@
 
 	// Your starting point. Enjoy the ride!
 	var myApp = angular.module('app',[]);
-	myApp.controller('MainController', ['$scope', function($scope){
+	myApp.controller('MainController', ['$scope','$location', function($scope,$location){
 		$scope.todos=[
 			{
 				id:0.123,
@@ -28,7 +28,72 @@
 				text: $scope.text,
 				completed:false
 			});
-		}	
+		};	
+		//remove
+		$scope.remove = function(id){
+			for(var i =0;i<$scope.todos.length;i++){
+				if($scope.todos[i].id == id){
+					$scope.todos.splice(i,1);
+				}
+			}
+		};
+		//clearCompleted
+		$scope.clearCompleted = function(){
+			var result = [];
+			for(var i=0;i<$scope.todos.length;i++){
+				if(!$scope.todos[i].completed){
+					result.push($scope.todos[i]);
+				}
+			}
+			$scope.todos = result;
+		};
+		//check whether to show "clear completed" button or not
+		$scope.existCompleted = function(){
+			for(var i=0;i<$scope.todos.length;i++){
+				if($scope.todos[i].completed){
+					return true;
+				}
+			}
+			return false;
+		};
+		//currentEditing
+		$scope.currentEditing = -1;
+		$scope.editing = function(id){
+			$scope.currentEditing = id;
+		};
+		//save editing text
+		$scope.save = function(){
+			$scope.currentEditing = -1;
+		};
+		//全选toggleAll
+		$scope.toggleAll = true;
+		$scope.toggle = function(){
+			for(var i=0;i<$scope.todos.length;i++){
+				$scope.todos[i].completed= $scope.toggleAll;
+			}
+			$scope.toggleAll = !$scope.toggleAll;
+		};
+
+		//filter All Active Completed
+		$scope.selector = {};
+		$scope.location = $location;
+		//watch 只能监视属于$scope的成员 
+		$scope.$watch('location.path()', function(now, old){
+			//console.log(now);
+			switch(now){
+		        case '/active':
+        			$scope.selector={ completed : false };
+        			break;
+        		case '/completed':
+        			$scope.selector={ completed : true };
+        			break;
+        		default:
+        			$scope.selector={};
+        			break;
+			}
+		});
+
+
 
 	}]);
 })(angular);
--- todomvc-app-template-master/readme.md
+++ todomvc-app-template-master/readme.md
@@ -5,6 +5,6 @@
 4. clear completed; 如果 existCompleted 为false， 不显示clear completed
 5. 当前编辑; 回车保存
 6. (左上角)全选
-7. filter all active completed
+7. filter all active completed ($location实现)
 8. route 路由 
 9. service 服务
